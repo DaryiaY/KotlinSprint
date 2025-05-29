8428463fe25bf7d4e7e79fa6f87901007f6e056e
daryia <sunnydasha001@gmail.com>
2025-05-30 00:48:16 +0200

    KS-1-3 declaration and print of variables

---
+++ app/src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.app
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("year flight = $year")
+    println("hour takeoff = $hour")
+    println("minute takeoff = $minute")
+
+    hour = 10
+    minute = 55
+
+    println("landing = $hour:$minute")
+}
