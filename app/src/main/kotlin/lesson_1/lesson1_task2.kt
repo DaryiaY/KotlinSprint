aa2405c287f0fc092e1e457a641699e6884582cb
daryia <sunnydasha001@gmail.com>
2025-05-30 00:09:08 +0200

    KS-1-2 declaration and print of variables

---
+++ app/src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,15 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textGratitude: String = "Спасибо, что выбрали нас!"
+
+    println("numberOfOrders = $numberOfOrders")
+    println("textGratitude = $textGratitude")
+
+    var numberOfEmployees: Int = 2000
+    //println("numberOfEmployees = $numberOfEmployees")
+
+    numberOfEmployees = 1999
+    println("numberOfEmployees = $numberOfEmployees")
+}
