5f906b5618e89bc6b1d5cbeb19c2b7d52d2bb4d2
daryia <sunnydasha001@gmail.com>
2025-05-30 10:26:43 +0200

    KS-1-3 changed the type of variables

--- app/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,15 +2,15 @@ package org.example.app.lesson_1
 
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println("year flight = $year")
     println("hour takeoff = $hour")
     println("minute takeoff = $minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("landing = $hour:$minute")
 }
