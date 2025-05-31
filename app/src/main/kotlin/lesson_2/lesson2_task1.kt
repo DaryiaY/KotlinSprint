6440b7485a0e7dde1c0a065c32c04ffccab5d543
daryia <sunnydasha001@gmail.com>
2025-05-31 09:18:14 +0200

    KS-2-1 added a variable

--- app/src/main/kotlin/lesson_2/lesson2_task1.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,8 +5,9 @@ fun main() {
     val student2: Double = 4.0
     val student3: Double = 3.0
     val student4: Double = 5.0
+    val countStudents: Int = 4
 
-    val sumAndDiv: Double = (student1 + student2 + student3 + student4) / 4
+    val sumAndDiv: Double = (student1 + student2 + student3 + student4) / countStudents
     val averageScore: String = String.format("%.2f", sumAndDiv)
 
     println("Средний балл = $averageScore")
