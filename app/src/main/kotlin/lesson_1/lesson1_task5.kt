a92454cbdf0531500d65eb77a37fffff2098d3ae
daryia <sunnydasha001@gmail.com>
2025-05-31 09:10:49 +0200

    KS-1-5 added constants and adjusted print

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package org.example.app.lesson_1
 
+const val COUNT_SEC_IN_MIN = 60
+const val COUNT_MIN_IN_HOUR = 60
 fun main() {
     val totalTime: Int = 6480
-    val sec: Int = totalTime % 60
-    val min: Int = (totalTime / 60) % 60
-    val hours: Int = (totalTime / 60) / 60
+    val sec: Int = totalTime % COUNT_SEC_IN_MIN
+    val min: Int = (totalTime / COUNT_SEC_IN_MIN) % COUNT_SEC_IN_MIN
+    val hours: Int = (totalTime / COUNT_SEC_IN_MIN) / COUNT_MIN_IN_HOUR
 
-    println("${String.format("%02d", hours)}:${String.format("%02d", min)}:${String.format("%02d", sec)}")
+    println("%02d:%02d:%02d".format(hours, min, sec))
 }
